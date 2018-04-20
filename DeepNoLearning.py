d6f73b026bc7bc3087b16eb2e978aacc4b034110
Longfei Xie <896161120@qq.com>
2018-04-20 20:31:50 +0800

    The Parent class definition

---
+++ DeepNoLearning.py
@@ -0,0 +1,7 @@
+class DeepNoLearning:
+
+    # Select one model and run on the real platform
+    def model_run(self): pass
+
+    # Select one model and construct it
+    def model_construct(self): pass
